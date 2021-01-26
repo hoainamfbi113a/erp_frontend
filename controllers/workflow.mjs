b587a10e1ba9e1751b4591d793eadad1dda4e970
namnh <49768691+hoainamfbi113a@users.noreply.github.com>
2021-01-26 18:15:19 +0700

    fix base url

--- controllers/workflow.mjs
+++ controllers/workflow.mjs
@@ -10,17 +10,17 @@ router.get('/update-profile',async (req,res)=>{
     res.send(data);
 })
 router.get('/get-workflow-types', async (req,res)=>{
-  let { data } = await axios.get(`https://workflow.tuoitre.vn/api/workflow/get-workflow-types`);
+  let { data } = await axios.get(`${process.env.apiWorkflow}/api/workflow/get-workflow-types`);
     
     res.send(data);
 })
 router.get('/detail', async (req,res)=>{
   const { type } = req.query;
-  let { data } = await axios.get(`https://workflow.tuoitre.vn/api/workflow/detail?type=${type}`);
+  let { data } = await axios.get(`${process.env.apiWorkflow}/api/workflow/detail?type=${type}`);
   res.send(data);
 })
 router.get('/store', async (req,res)=>{
-  let { data } = await axios.get(`https://workflow.tuoitre.vn/api/workflow/store`);
+  let { data } = await axios.get(`${process.env.apiWorkflow}/api/workflow/store`);
     
     res.send(data);
 })
