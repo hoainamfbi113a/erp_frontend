43e568e6a443be8ffba75da05c40aeb7b1c883d0
namnh <49768691+hoainamfbi113a@users.noreply.github.com>
2021-03-25 14:26:13 +0700

    fix document

--- client/src/components/content/documents/Form.js
+++ client/src/components/content/documents/Form.js
@@ -1,209 +1,12 @@
 import React, { Component } from "react";
 import classes from "classnames";
-import * as Constant from "constant/ConstantDocument";
-import * as ApiHelper from "helpers/ApiHelper";
+
 import axios from "axios";
-import { withRouter } from "react-router-dom";
-import { Typography } from "antd";
-import { Radio } from "antd";
-const { Title } = Typography;
-import {
-  Steps,
-} from "antd";
+import { Steps } from "antd";
 const { Step } = Steps;
-import ModalForm from "./ModalForm"
-
-function RenderInputPreview(props) {
-  var data = "";
-  switch (props.data.type) {
-    case Constant.INPUT_TYPE_HEADER:
-      data = (
-        <div className="form-group">
-          {/* <Title level={props.data.subtype.slice(1, 2)}> */}
-          <Title level={5}>
-            {props.data.label}
-          </Title>
-        </div>
-      );
-      break;
-    case Constant.INPUT_TYPE_PARAGRAPH:
-      data = (
-        <div className="form-group">
-          <p>{props.data.label}</p>
-        </div>
-      );
-      break;
-    case Constant.INPUT_TYPE_TEXTFIELD:
-      data = (
-        <div className="form-group">
-          <label className="control-label">{props.data.label}</label>
-          <input
-            type="text"
-            name={props.data.name}
-            placeholder={props.data.placeHolder}
-            className="form-control"
-            onChange={(event) => props.handleChange(event, props.data.id)}
-            value={props.value}
-          />
-        </div>
-      );
-      break;
-    case Constant.INPUT_TYPE_AREA:
-      data = (
-        <div className="form-group">
-          <label className="control-label">{props.data.label}</label>
-          <textarea
-            rows="5"
-            value={props.value}
-            onChange={(event) => props.handleChange(event, props.data.id)}
-            name={props.data.name}
-            placeholder={props.data.placeHolder}
-            className="form-control"
-          />
-        </div>
-      );
-      break;
-    case Constant.INPUT_TYPE_SELECT:
-      data = (
-        <div className="form-group">
-          <label className="control-label">{props.data.label}</label>
-          <select
-            value={props.value}
-            onChange={(event) => props.handleChange(event, props.data.id)}
-            className="form-control"
-            name={props.data.name}
-          >
-            {typeof props.data.values !== "undefined" &&
-              props.data.values.length > 0 &&
-              props.data.values.map((item, index) => (
-                <option value={item.value}>{item.label}</option>
-              ))}
-          </select>
-        </div>
-      );
-      break;
-    case Constant.INPUT_TYPE_RADIO:
-      {
-        var selectedRadio = "";
-        for (let item of props.data.values) {
-          if (item.selected === true) {
-            selectedRadio = item.value;
-          }
-        }
-      }
-      data = (
-        <div className="form-group">
-          <label className="control-label">{props.data.label} </label>
-          <Radio.Group
-            style={{ display: "block" }}
-            name={props.data.name}
-            defaultValue={props.data.value ? props.data.value.toString():""}
-            onChange={(event) => props.handleChange(event, props.data.id)}
-          >
-            {typeof props.data.values !== "undefined" &&
-              props.data.values.length > 0 &&
-              props.data.values.map((item, index) => (
-                <Radio style={{ display: "block" }} value={item.value}>
-                  {item.label}
-                </Radio>
-              ))}
-          </Radio.Group>
-        </div>
-      );
-      break;
-    case Constant.INPUT_TYPE_CHECKBOX:
-      data = (
-        <div className="form-group">
-          <label className="control-label">{props.data.label}</label>
-          {typeof props.data.values !== "undefined" &&
-            props.data.values.length > 0 &&
-            props.data.values.map((item, index) => (
-              <div className="form-check">
-                <label className="form-check-label">
-                  <input
-                    type="checkbox"
-                    // defaultChecked={item.selected}
-                    checked={
-                      props.value && props.value.length > 0 && props.value.includes(item.value)
-                    }
-                    onChange={(event) =>
-                      props.handleCheckboxChange(event, props.data.id)
-                    }
-                    className="form-check-input"
-                    name={props.data.name}
-                    value={item.value}
-                  />
-                  {item.label}
-                </label>
-              </div>
-            ))}
-        </div>
-      );
-      break;
-    case Constant.INPUT_TYPE_EMAIL:
-      data = (
-        <div className="form-group">
-          <label className="control-label">{props.data.label}</label>
-          <input
-            type="email"
-            onChange={(event) => props.handleChange(event, props.data.id)}
-            name={props.data.name}
-            placeholder={props.data.placeHolder}
-            className="form-control"
-          />
-        </div>
-      );
-      break;
-    case Constant.INPUT_TYPE_NUMBER:
-      data = (
-        <div className="form-group">
-          <label className="control-label">{props.data.label}</label>
-          <input
-            type="number"
-            onChange={(event) => props.handleChange(event, props.data.id)}
-            name={props.data.name}
-            placeholder={props.data.placeHolder}
-            className="form-control"
-          />
-        </div>
-      );
-      break;
-    case Constant.INPUT_TYPE_PASSWORD:
-      data = (
-        <div className="form-group">
-          <label className="control-label">{props.data.label}</label>
-          <input
-            type="password"
-            onChange={(event) => props.handleChange(event, props.data.id)}
-            name={props.data.name}
-            placeholder={props.data.placeHolder}
-            className="form-control"
-          />
-        </div>
-      );
-      break;
-    case Constant.INPUT_TYPE_DATE:
-      data = (
-        <div className="form-group">
-          <label className="control-label">{props.data.label}</label>
-          <input
-            type="date"
-            onChange={(event) => props.handleChange(event, props.data.id)}
-            name={props.data.name}
-            value ={props.data.value}
-            className="form-control"
-          />
-        </div>
-      );
-      break;
-    // case Constant.INPUT_TYPE_BUTTON :
-    //     data = <button onClick={props.handleClick} name={props.data.name} type="submit" className={props.data.btnClass}>{props.data.btnValue}</button>
-    //     break;
-    default:
-  }
-  return data;
-}
-
+import RenderInputPreview from "./Input";
+import docCookies from "doc-cookies";
+import axiosConfig from "apis/axios";
 class Create extends Component {
   constructor(props) {
     super(props);
@@ -214,11 +17,72 @@ class Create extends Component {
       create: true,
       valueRadio: 1,
       showModal: false,
-      dataForm:null,
-      dataWorkFlow:null,
-      currentProcess:0,
+      dataForm: null,
+      dataWorkFlow: null,
+      currentProcess: 0,
     };
   }
+  componentDidMount = () => {
+    const id = this.props.match.params.id;
+    try {
+      if (this.state.create === false) {
+        axios
+          .get(`/api/document-process/get?process_id=${id}`)
+          .then((res) => {
+            let incr = 0;
+            this.setState({
+              currentProcess: res.data.current_step.id,
+            });
+          })
+          .catch((err) => {});
+        axios
+          .get(`/api/document/get?id=${id}`)
+          .then((res) => {
+            this.setState({
+              listInputs: res.data.inputs,
+              inputsData: res.data.inputs,
+            });
+            axios
+              .get(`/api/workflow/detail?type_id=${res.data.document_type.id}`)
+              .then((res) => {
+                this.setState({
+                  dataWorkFlow: res.data,
+                });
+              })
+              .catch((err) => {});
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } else {
+        let params = {
+          type_id: 2,
+        };
+        axios
+          .get(`/api/document-template/get?type_id=${id}`)
+          .then((data) => {
+            if (data.data.inputs.length === 0) {
+              alert("Template chưa được tạo");
+              this.props.history.push(`/notification/create`);
+            }
+            this.setState({
+              listInputs: data.data.inputs,
+            });
+            axios
+              .get(`/api/workflow/detail?type_id=${data.data.document_type.id}`)
+              .then((res) => {
+                this.setState({
+                  dataWorkFlow: res.data,
+                });
+              })
+              .catch((err) => {});
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    } catch (error) {}
+  };
   handleCheckboxChange = (e, inputId) => {
     const stateInputsData = this.state.inputsData;
     var index = stateInputsData.findIndex((x) => x.id === inputId);
@@ -302,112 +166,56 @@ class Create extends Component {
       });
     }
   }
-  componentDidMount = () => {
-    const id = this.props.match.params.id;
-    try {
-      if (this.state.create === false) {
-        axios.get(`/api/document-process/get?process_id=${id}`)
-        .then(res=>{
-          let incr = 0
-          this.setState({
-            currentProcess: res.data.current_step.id
-          })
-        })
-        .catch(err=>{
-        })
-        axios
-          .get(`/api/document/get?id=${id}`)
-          .then((res) => {
-            this.setState({
-              listInputs: res.data.inputs,
-              inputsData: res.data.inputs,
-            });
-            axios.get(`/api/workflow/detail?type_id=${res.data.document_type.id}`)
-            .then(res=>{
-              this.setState({
-                dataWorkFlow:res.data
-              })
-            })
-            .catch(err=>{
-  
-            })
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      } else {
-        let params = {
-          type_id: 2,
-        };
-        axios
-          .get(
-            `/api/document-template/get?type_id=${id}`
-          )
-          .then((data) => {
-            if (data.data.inputs.length === 0) {
-              alert("Template chưa được tạo");
-              this.props.history.push(`/notification/create`);
-            }
-            this.setState({
-              listInputs: data.data.inputs,
-            });
-            axios.get(`/api/workflow/detail?type_id=${data.data.document_type.id}`)
-            .then(res=>{
-              this.setState({
-                dataWorkFlow:res.data
-              })
-            })
-            .catch(err=>{
-  
-            })
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
-    } catch (error) {
-      
-    }
-  
-  };
+
   handleSubmit = (e) => {
     if (this.state.create === true) {
-      this.setState({
-        showModal:true
-      })
-      let dataForm = {
-        type_id: this.props.match.params.id,
-        user_id: 1,
-        inputs: this.state.inputsData,
+      let params = {
+        document_type_id: this.props.match.params.id,
+        user_id: docCookies.getItem("user_id"),
+        dataWorkFlow:this.state.dataWorkFlow,
+        inputsData: this.state.inputsData,
+        // targets: targets,
       };
-      this.setState({
-        dataForm
-      })
+      axiosConfig
+        .post("/api/document/store", params)
+        .then((data) => {
+          console.log(data)
+          if(data === "success") {
+            alert("gửi tài liệu thành công")
+          } else {
+            alert("gửi tài liệu thất bại")
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      
     } else {
-        let arrValueInput = []
-        for( let item of this.state.inputsData){
-          if(item.value === null){
-            item.value = "null"
-          }
-          let obj = {
-            id:item.id,
-            value:item.value
-          }
-          arrValueInput.push(obj)
+      let arrValueInput = [];
+      for (let item of this.state.inputsData) {
+        if (item.value === null) {
+          item.value = "null";
         }
-        let data = {
-          document_id: this.props.match.params.id,
-          user_id: 1,
-          inputs: arrValueInput,
-        }
-        axios.post("https://document.tuoitre.vn/api/document/update", data)
-        .then(res=>{
-          alert("update document thành công")
+        let obj = {
+          id: item.id,
+          value: item.value,
+        };
+        arrValueInput.push(obj);
+      }
+      let data = {
+        document_id: this.props.match.params.id,
+        user_id: 1,
+        inputs: arrValueInput,
+      };
+      axios
+        .post("https://document.tuoitre.vn/api/document/update", data)
+        .then((res) => {
+          alert("update document thành công");
         })
-        .catch(err=>{
-          console.log(err)
-          alert("update thất bại")
-        })
+        .catch((err) => {
+          console.log(err);
+          alert("update thất bại");
+        });
     }
 
     // }
@@ -418,30 +226,27 @@ class Create extends Component {
         return <Step key={item.id} title={item.description} />;
       });
     } else {
-
     }
     // return ""
   };
   render() {
-   
     const { listInputs, inputsData } = this.state;
     return (
       <div>
         <div className="row">
-        <Steps current={0} size="small" className="process-work-flow">
+          <Steps current={0} size="small" className="process-work-flow">
             {this.renderWorkflow()}
             <Step title="Tài liệu sẵn sàng" />
-        </Steps>
-          <div className="col-md-2" style ={{margin:"26px"}}>
-          {
-          this.state.create === true &&
-            <span
-              className="btn-add-user"
-              onClick={(e) => this.handleSubmit(e)}
-            >
-              Lưu tài liệu
-            </span>
-          }
+          </Steps>
+          <div className="col-md-2" style={{ margin: "26px" }}>
+            {this.state.create === true && (
+              <span
+                className="btn-add-user"
+                onClick={(e) => this.handleSubmit(e)}
+              >
+                Gửi tài liệu
+              </span>
+            )}
             <span
               className="btn-add-user"
               onClick={() => this.props.history.goBack()}
@@ -487,12 +292,12 @@ class Create extends Component {
               })}
           </div>
         </div>
-        <ModalForm dataForm = {this.state.dataForm} idWorkflow ={this.props.match.params.id} show ={this.state.showModal} hideModal = {()=>{
+        {/* <ModalForm dataForm = {this.state.dataForm} idWorkflow ={this.props.match.params.id} show ={this.state.showModal} hideModal = {()=>{
           this.setState({
             showModal:false
           })
         }}
-        />
+        /> */}
       </div>
     );
   }
--- controllers/FormBuilder.mjs
+++ controllers/FormBuilder.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import axios from "axios"
+import axios from "axios";
 const router = express.Router();
 router.get("/document-type/get-document-types", async (req, res) => {
   try {
@@ -8,21 +8,19 @@ router.get("/document-type/get-document-types", async (req, res) => {
     );
     res.send(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-  
 });
 router.get("/document-template/get", async (req, res) => {
-  let {type_id} = req.query;
+  let { type_id } = req.query;
   try {
     let { data } = await axios.get(
       `${process.env.apiFormBuilder}/api/document-template/get?type_id=${type_id}`
     );
     res.send(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-  
 });
 router.post("/document-template/store", async (req, res) => {
   try {
@@ -32,34 +30,31 @@ router.post("/document-template/store", async (req, res) => {
     );
     res.send(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
- 
 });
 router.get("/document/get", async (req, res) => {
-  let {id } = req.query;
+  let { id } = req.query;
   try {
     let { data } = await axios.get(
       `${process.env.apiFormBuilder}/api/document/get?id=${id}`
     );
     res.send(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-  
 });
 
-
 router.get("/document/list", async (req, res) => {
-  let {page,per_page,user_id } = req.query;
+  let { page, per_page, user_id } = req.query;
   try {
     let { data } = await axios.get(
       `${process.env.apiFormBuilder}/api/document/list?page=${page}&per_page=${per_page}&user_id=${user_id}`
     );
     res.send(data);
-    console.log(data)
+    console.log(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 });
 
@@ -70,22 +65,107 @@ router.get("/document-type/get-document-types", async (req, res) => {
     );
     res.send(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 });
+let getTarget = async (pos_id, dep_id, step_id, action_id) => {
+  // console.log(dep_id, pos_id)
+  let target = [];
+  let { data } = await axios.get(
+    `https://employee.tuoitre.vn/api/departments/positions/list-user/${dep_id}?order=asc&pos_id=${pos_id}`
+  );
+  const object = data;
+  for (const property in object.data) {
+    let obj = {
+      target_id: object.data[property].id,
+      target_name: object.data[property].full_name,
+      step_id,
+      action_id,
+    };
+    target.push(obj);
+  }
+  return target;
+};
 
 router.post("/document/store", async (req, res) => {
-  console.log(req.body)
-  try {
-    let { data } = await axios.post(
-      `${process.env.apiFormBuilder}/api/document/store`,
-      req.body
-    );
-    res.send(data);
-  } catch (error) {
-    console.log(error)
+  // let isSuccess = false;
+  let resEnd = res;
+  let target = [];
+  // console.log("123")
+  let { document_type_id, user_id, dataWorkFlow, inputsData } = req.body;
+  const config = {
+    headers: { Authorization: req.headers.authorization },
+  };
+  //get pos, dep of user
+  let pos_idUser, dep_idUser;
+
+  let { data } = await axios.get(
+    `${process.env.apiEmployee}/api/fe/profiles/users/${user_id}`,
+    config
+  );
+  pos_idUser = data.data.department.data.dep_id;
+  dep_idUser = data.data.department.data.pos_id;
+  for (let item of dataWorkFlow.steps) {
+    let pos_id, dep_id;
+    dep_id =
+      item.actions[0].department_id == null
+        ? dep_idUser
+        : item.actions[0].department_id;
+    pos_id =
+      item.actions[0].position_id == null
+        ? pos_idUser
+        : item.actions[0].position_id;
+    let arrChild = await getTarget(pos_id, dep_id, item.id, item.actions[0].id);
+    target = [...target, ...arrChild];
   }
- 
+  let paramsIssue = {
+    document_type_id,
+    user_id,
+    targets: target,
+  };
+  axios
+    .post(`${process.env.apiWorkflow}/api/issue/store`, paramsIssue)
+    .then((res1) => {
+      let dataForm = {
+        type_id: document_type_id,
+        user_id: user_id,
+        inputs: inputsData,
+      };
+      dataForm.issue_id = res1.data.id;
+      axios
+        .post(`https://document.tuoitre.vn/api/document/store`, dataForm)
+        .then((res) => {
+          let params = {
+            document_id: res.data.id,
+            issue_id: res1.data.id,
+          };
+
+          axios
+            .post(
+              "https://document.tuoitre.vn/api/document-process/store",
+              params
+            )
+            .then((res3) => {
+              console.log("success2");
+              resEnd.send("success")
+            })
+            .catch((err) => {
+              console.log("err1");
+            });
+        })
+        .catch((err) => {
+          // console.log("err")
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  // console.log(isSuccess);
+  // if (isSuccess === true) {
+  //   res.send("success");
+  // } else {
+  //   res.send("failed");
+  // }
 });
 
 router.post("/api/document/update", async (req, res) => {
@@ -96,9 +176,8 @@ router.post("/api/document/update", async (req, res) => {
     );
     res.send(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
- 
 });
 
 router.delete("/api/document/delete", async (req, res) => {
@@ -109,12 +188,9 @@ router.delete("/api/document/delete", async (req, res) => {
     );
     res.send(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
- 
 });
-
-
 
 router.post("/document-type/store", async (req, res) => {
   try {
@@ -124,9 +200,8 @@ router.post("/document-type/store", async (req, res) => {
     );
     res.send(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
- 
 });
 
 router.delete("/document-type/delete", async (req, res) => {
@@ -137,21 +212,20 @@ router.delete("/document-type/delete", async (req, res) => {
     );
     res.send(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
- 
 });
 
 router.get("/document-process/get", async (req, res) => {
-  let {process_id } = req.query;
+  let { process_id } = req.query;
   try {
     let { data } = await axios.get(
       `${process.env.apiFormBuilder}/api/document-process/get?process_id=${process_id}`
     );
     res.send(data);
-    console.log(data)
+    console.log(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 });
 
