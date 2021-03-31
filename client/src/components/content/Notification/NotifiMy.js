13fcaa00a1d8b90fc7d35c7e9e72acc835cc732c
Bảo Xuân <baoxuan@baos-mbp.tuoitre.news>
2021-03-31 15:02:21 +0700

    fix notiDoc

--- client/src/components/content/Notification/NotifiMy.js
+++ client/src/components/content/Notification/NotifiMy.js
@@ -1,36 +1,38 @@
-import React, { Component } from "react";
-import { Button, notification, Pagination } from "antd";
+import React, { useEffect, useState } from "react";
 import docCookies from "doc-cookies";
 import { updateStatusNotify, listNotify } from "apis/notificationApi";
 import axiosConfig from "apis/axios";
+import { simpleDate } from "../../../helpers/FuncHelper"
+import { Button, Pagination, Tag } from "antd";
+import {
+  CheckCircleOutlined,
+  SyncOutlined,
+} from '@ant-design/icons';
 import "./notification.css";
 const app_id = 99;
 const slug = "profile";
 const per_page = 15;
-export default class NotifiMy extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      paginationPage: 1,
-      notification: null,
-    };
-  }
-  componentDidMount = () => {
+
+const NotifiMy = (props) => {
+
+  const [data, setData] = useState(null);
+  const [pagination, setPage] = useState(1);
+  const [noti, setNoti] = useState(null);
+
+  useEffect(() => {
     const user_id = docCookies.getItem("user_id");
     axiosConfig
       .get(`/api/notification/list?user_id=${user_id}`)
-      .then((res) => {
-        this.setState({
-          notification: res.data,
-        });
-      })
-      .catch((err) => {
-        console.log("err");
-      });
-    // this.fetchNotify(1);
-  };
-  fetchNotify = async (value) => {
+        .then((res) => {
+          console.log(res);
+          setNoti(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+  }, []);
+
+  const fetchNotify = async (value) => {
     const user_id = docCookies.getItem("user_id");
     const params = {
       app_id,
@@ -41,65 +43,65 @@ export default class NotifiMy extends Component {
     };
     let resListNotify = await listNotify(params);
     if (!resListNotify.err) {
-      this.setState({
-        data: resListNotify,
-      });
+      setData(resListNotify);
     }
   };
 
-  changeStatusNotify = async (id) => {
+  const changeStatusNotify = async (id) => {
     const params = {
       status: "2",
     };
     let resUpdateStatusNotify = await updateStatusNotify(id, params);
     if (resUpdateStatusNotify.message === "Success!. Updated") {
-      this.fetchNotify(this.state.paginationPage);
+      fetchNotify(pagination);
     }
   };
-  onChangePagination = (page) => {
-    this.setState({
-      paginationPage: page,
-    });
-    this.fetchNotify(page);
+
+  const onChangePagination = (page) => {
+    setPage(page)
+    fetchNotify(page);
   };
-  changeStatusNotiDocument = (item_id, document_id, process_id) => {
+
+  const changeStatusNotiDocument = (item_id, document_id, process_id) => {
     axiosConfig
       .get(`/api/notification/mark-as-read/${item_id}`)
-      .then((res) => {
-        axiosConfig
-          .get(`/api/document-process/get?id=${process_id}`)
-          .then((res) => {
-            if (res.targets[0].target_id == docCookies.getItem("user_id")) {
-              let body = {
-                process_id: +process_id,
-                user_id: +docCookies.getItem("user_id"),
-                status: "view",
-              };
-              axiosConfig
-                .post("/api/document-process/process", body)
-                .then((res) => {
-                })
-                .catch((err) => {
-                  console.log("loi roi");
-                  console.log(err)
-                });
-            }
-          })
-          .catch((err) => {
-            console.log("erraaaa");
-          });
-      })
-      .catch((err) => {
-        console.log("err", err);
-      });
-      this.props.history.push(
+        .then((res) => {
+          axiosConfig
+            .get(`/api/document-process/get?id=${process_id}`)
+            .then((res) => {
+              if (res.targets[0].target_id == docCookies.getItem("user_id")) {
+                let body = {
+                  process_id: +process_id,
+                  user_id: +docCookies.getItem("user_id"),
+                  status: "view",
+                };
+                axiosConfig
+                  .post("/api/document-process/process", body)
+                  .then((res) => {
+                  })
+                  .catch((err) => {
+                    console.log("loi roi");
+                    console.log(err)
+                  });
+              }
+            })
+            .catch((err) => {
+              console.log("erraaaa");
+            });
+        })
+        .catch((err) => {
+          console.log("err", err);
+        });
+      props.history.push(
         `/form-document-view/${document_id}/${process_id}`
       );
   };
-  renderNotifyItemDocument = () => {
-    let dataNotify = this.state.notification;
-    if (!!dataNotify) {
+
+  const renderNotifyItemDocument = () => {
+    let dataNotify = noti;
+    if (dataNotify) {
       return dataNotify.map((item) => {
+        const status = item.process.status === "processed";
         return (
           <tr key={item.id}>
             <td>
@@ -107,28 +109,31 @@ export default class NotifiMy extends Component {
                 {item.document_type.display_name}
               </div>
             </td>
-            {console.log(item)}
+            <td>{item.user_name}</td>
+            <td>{item.department_name}</td>
             <td
               className={item.read_at === null ? "content-notification-unread" : ""}
               onClick={() => {
-                this.changeStatusNotiDocument(
+                changeStatusNotiDocument(
                   item.id,
                   item.process.document_id,
                   item.process.id
                 );
               }}
             >
-              {item.content} {item.document_type.display_name} <b> {item.user_name} </b> {item.department_name}
+              {item.document_type.display_name}
             </td>
-            <td>{item.updated_at}</td>
+            <td><Tag icon={status ? <CheckCircleOutlined /> : <SyncOutlined spin />} color={status ? "success" : "processing" }>
+              {status ? `Đã duyệt` : `Đang chờ duyệt` }</Tag></td>
+            <td>{simpleDate(item.created_at)}</td>
           </tr>
         );
       });
     }
   };
 
-  renderNotifyItem = () => {
-    let dataNotifyDocument = this.state.data;
+  const renderNotifyItem = () => {
+    let dataNotifyDocument = data;
     if (!!dataNotifyDocument) {
       return dataNotifyDocument.data.map((item) => {
         return (
@@ -143,7 +148,7 @@ export default class NotifiMy extends Component {
             <td
               className={item.status === 1 ? "content-notification-unread" : ""}
               onClick={() => {
-                this.changeStatusNotify(item.id);
+                changeStatusNotify(item.id);
               }}
             >
               {item.content}
@@ -155,89 +160,93 @@ export default class NotifiMy extends Component {
     }
   };
 
-  render() {
-    return (
-      <div className="content-background2">
-        <div style={{ minHeight: "70vh" }} className="content-main">
-          <div className="content-top">
-            <div className="content-top-left content-notification">
-              <div className="content-top-left-sum-item">Thông báo của tôi</div>
-              <Button className="btn-notification" type="primary">
-                ...
-              </Button>
-            </div>
-          </div>
-          <table className="content-notification-table">
-            <tbody>
-              <tr>
-                <th>Tin</th>
-                <th>Nội dung</th>
-                <th>Ngày</th>
-              </tr>
-              {/* <tr>
-                <td>
-                  <div className="content-notification-table-btn content-notification-table-btn__paycheck">
-                    Phiếu lương
-                  </div>
-                </td>
-                <td className="content-notification-unread">
-                  Gửi Vinh, Phiếu lương tháng 11/2020...
-                </td>
-                <td>09:15</td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="content-notification-table-btn">Họp</div>
-                </td>
-                <td className="content-notification-unread">
-                  Gửi Vinh, Họp phòng CNTT...
-                </td>
-                <td>09:15</td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="content-notification-table-btn">Họp</div>
-                </td>
-                <td className="content-notification-unread">
-                  Gửi Vinh, Họp chi đoàn tòa soạn.
-                </td>
-                <td>09:15</td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="content-notification-table-btn">Họp</div>
-                </td>
-                <td>
-                  Gửi các bạn phòng CNTT, 2:00 PM Phòng CNTT sẽ có cuộc hơp về
-                  dự án A với sự miêu tả của anh B. Mong các bạn đến đúng
-                  giờ....
-                </td>
-                <td>09:15</td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="content-notification-table-btn">Họp</div>
-                </td>
-                <td>
-                  Gửi các bạn phòng CNTT, 2:00 PM Phòng CNTT sẽ có cuộc hơp về
-                  dự án A với sự miêu tả của anh B. Mong các bạn đến đúng
-                  giờ....
-                </td>
-                <td>09:15</td>
-              </tr> */}
-              {this.renderNotifyItem()}
-              {this.renderNotifyItemDocument()}
-            </tbody>
-          </table>
-          <div>
-            <Pagination
-              defaultCurrent={1}
-              total={50}
-              onChange={this.onChangePagination}
-            />
+  return (
+    <div className="content-background2">
+      <div style={{ minHeight: "70vh" }} className="content-main">
+        <div className="content-top">
+          <div className="content-top-left content-notification">
+            <div className="content-top-left-sum-item">Thông báo của tôi</div>
+            <Button className="btn-notification" type="primary">
+              ...
+            </Button>
           </div>
         </div>
+        <table className="content-notification-table">
+          <tbody>
+            <tr>
+              <th>Đơn</th>
+              <th>Người gửi</th>
+              <th>Phòng ban</th>
+              <th>Nội dung</th>
+              <th>Trạng thái</th>
+              <th>Ngày</th>
+            </tr>
+            {/* <tr>
+              <td>
+                <div className="content-notification-table-btn content-notification-table-btn__paycheck">
+                  Phiếu lương
+                </div>
+              </td>
+              <td className="content-notification-unread">
+                Gửi Vinh, Phiếu lương tháng 11/2020...
+              </td>
+              <td>09:15</td>
+            </tr>
+            <tr>
+              <td>
+                <div className="content-notification-table-btn">Họp</div>
+              </td>
+              <td className="content-notification-unread">
+                Gửi Vinh, Họp phòng CNTT...
+              </td>
+              <td>09:15</td>
+            </tr>
+            <tr>
+              <td>
+                <div className="content-notification-table-btn">Họp</div>
+              </td>
+              <td className="content-notification-unread">
+                Gửi Vinh, Họp chi đoàn tòa soạn.
+              </td>
+              <td>09:15</td>
+            </tr>
+            <tr>
+              <td>
+                <div className="content-notification-table-btn">Họp</div>
+              </td>
+              <td>
+                Gửi các bạn phòng CNTT, 2:00 PM Phòng CNTT sẽ có cuộc hơp về
+                dự án A với sự miêu tả của anh B. Mong các bạn đến đúng
+                giờ....
+              </td>
+              <td>09:15</td>
+            </tr>
+            <tr>
+              <td>
+                <div className="content-notification-table-btn">Họp</div>
+              </td>
+              <td>
+                Gửi các bạn phòng CNTT, 2:00 PM Phòng CNTT sẽ có cuộc hơp về
+                dự án A với sự miêu tả của anh B. Mong các bạn đến đúng
+                giờ....
+              </td>
+              <td>09:15</td>
+            </tr> */}
+            {renderNotifyItem()}
+            {renderNotifyItemDocument()}
+          </tbody>
+        </table>
+        <div>
+          <Pagination
+            defaultCurrent={1}
+            total={50}
+            onChange={onChangePagination}
+          />
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default NotifiMy;
+
