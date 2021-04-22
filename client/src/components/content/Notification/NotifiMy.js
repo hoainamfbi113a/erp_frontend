86d2fe9b7c11bf4202977ef4c605d3300c7d1502
Bảo Xuân <baoxuan@baos-mbp.tuoitre.news>
2021-04-22 13:59:04 +0700

    Fix noti2

--- client/src/components/content/Notification/NotifiMy.js
+++ client/src/components/content/Notification/NotifiMy.js
@@ -162,8 +162,7 @@ const NotifiMy = (props) => {
                 {item.document_type.display_name}
               </div>
             </td>
-            <td>{item.from_users ? item.from_users[0].target_name : ""}</td>
-            <td>{item.from_users ? item.from_users[0].department_name : ""}</td>
+            <td>{item.from_users ? item.from_users[0].target_name + " - " + item.from_users[0].department_name: ""}</td>
             <td>{item.title}</td>
             <td>{simpleDate(item.created_at)}</td>
           </tr>
@@ -213,9 +212,7 @@ const NotifiMy = (props) => {
             <tr>
               <th>Đơn</th>
               <th>Người gửi</th>
-              <th>Phòng ban</th>
               <th>Nội dung</th>
-              <th>Trạng thái</th>
               <th>Ngày</th>
             </tr>
             {renderNotifyItem()}
