84a7858a3cd13fb43e63e898c22cccd90b7e175a
namnh <49768691+hoainamfbi113a@users.noreply.github.com>
2021-06-14 10:32:14 +0700

    fix

--- client/src/components/admin/AddAndUpdateInforUser/Container/JoinDCSContainer.jsx
+++ client/src/components/admin/AddAndUpdateInforUser/Container/JoinDCSContainer.jsx
@@ -9,7 +9,7 @@ import {
 import moment from "moment";
 import JoinDCS from "../JoinDCS";
 const dateFormatList = ["DD/MM/YYYY", "DD/MM/YY"];
-import { formatDateNumber } from "../../../../helpers/FuncHelper";
+import { formatDateNumber, validateOnlyNumber } from "../../../helpers/FuncHelper";
 const JoinDCSContainer = (props) => {
   const [id, setId] = useState("");
   const dataJoinDCS = useSelector((state) => state.joinDCSUser);
@@ -33,10 +33,6 @@ const JoinDCSContainer = (props) => {
     par_issue_date:"",
     par_issue_committee:"",
   });
-  // const dataJoinDCS = useSelector((state) => state.joinDCSUser);
-  // console.log(dataJoinDCS)
-  // const [joinDCS, setJoinDCS] = useState(dataJoinDCS);
-  // console.log(joinDCS)
   const [visible, setVisible] = useState(false);
   const [refresh, setRefresh] = useState(true);
   const dispatch = useDispatch();
@@ -169,49 +165,55 @@ const JoinDCSContainer = (props) => {
       par_issue_date,
       par_issue_committee,
     } = joinDCS;
-    let date1 = moment(par_admission_date, "DD-MM-YYYY");
-    let date2 = moment(par_announcement_date, "DD-MM-YYYY");
-    let date3 = moment(par_issue_date, "DD-MM-YYYY");
-    const parsePar_admission_date = Date.parse(date1) / 1000;
-    const parsePar_announcement_date = Date.parse(date2) / 1000;
-    const parsePar_issue_date = Date.parse(date3) / 1000;
-    const params = {
-      pro_id: props.dataProfile.id,
-      user_id: props.idUser,
-      par_admission_date: par_admission_date,
-      par_branch,
-      par_committee,
-      par_first_full_name,
-      par_first_position,
-      par_first_workplace,
-      par_first_residence,
-      par_second_full_name,
-      par_second_position,
-      par_second_workplace,
-      par_second_residence,
-      par_announcement_date: par_announcement_date,
-      par_announcement_branch,
-      par_announcement_committee,
-      par_member_id,
-      par_issue_date: par_issue_date,
-      par_issue_committee,
-      id,
-    };
-    // console.log(params)
-    if (id == "") {
-      dispatch(addJoinDCS(params));
-      setTimeout(() => {
-        dispatch(
-          getJoinDCS({
-            id_user: props.idUser,
-            type: 1,
-          })
-        );
-      }, 900);
+    const validatePar = validateOnlyNumber(par_member_id)
+    if(validatePar === "") {
+      const validateOnlyNumber
+      let date1 = moment(par_admission_date, "DD-MM-YYYY");
+      let date2 = moment(par_announcement_date, "DD-MM-YYYY");
+      let date3 = moment(par_issue_date, "DD-MM-YYYY");
+      const parsePar_admission_date = Date.parse(date1) / 1000;
+      const parsePar_announcement_date = Date.parse(date2) / 1000;
+      const parsePar_issue_date = Date.parse(date3) / 1000;
+      const params = {
+        pro_id: props.dataProfile.id,
+        user_id: props.idUser,
+        par_admission_date: par_admission_date,
+        par_branch,
+        par_committee,
+        par_first_full_name,
+        par_first_position,
+        par_first_workplace,
+        par_first_residence,
+        par_second_full_name,
+        par_second_position,
+        par_second_workplace,
+        par_second_residence,
+        par_announcement_date: par_announcement_date,
+        par_announcement_branch,
+        par_announcement_committee,
+        par_member_id,
+        par_issue_date: par_issue_date,
+        par_issue_committee,
+        id,
+      };
+      if (id == "") {
+        dispatch(addJoinDCS(params));
+        setTimeout(() => {
+          dispatch(
+            getJoinDCS({
+              id_user: props.idUser,
+              type: 1,
+            })
+          );
+        }, 900);
+      } else {
+        dispatch(updateJoinDCS(params));
+        setId("");
+      }
     } else {
-      dispatch(updateJoinDCS(params));
-      setId("");
+      alert("Số đảng viên phải là số")
     }
+   
   };
   const onChangeBirthDay = (e, dateString, name) => {
     let date3 = moment(dateString, "DD-MM-YYYY");
