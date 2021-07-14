5fe53ede22dfe2c8ea9f47b6564e22ff6dfb5c83
Mac <mac@MacBook-Pro-cua-Mac.local>
2021-07-14 19:23:30 +0700

    fix

--- controllers/UserProfile/City.mjs
+++ controllers/UserProfile/City.mjs
@@ -4,7 +4,7 @@ import axios from "axios";
 const getAllCity = async (req, res) => {
   try {
     const { data } = await axios.get(
-      `${process.env.apiCity}/api/city`,
+      `https://thongtindoanhnghiep.co/api/city`,
     );
     res.send(data);
   } catch (error) {
@@ -16,7 +16,7 @@ const getAllDistrict = async (req, res) => {
   try {
     const { id } = req.params;
     const { data } = await axios.get(
-      `${process.env.apiCity}/api/city/${id}/district`,
+      `https://thongtindoanhnghiep.co/api/city/${id}/district`,
     );
     res.send(data);
   } catch (error) {
